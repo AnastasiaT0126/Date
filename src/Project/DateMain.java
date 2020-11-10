31ef8a558ead39b7f506cad4852bc43b9a8ef96a
Anastasia Todorova <anito0401@gmail.com>
2020-11-10 21:05:58 +0200

    DateMain

--- src/Project/DateMain.java
+++ src/Project/DateMain.java
@@ -1,6 +1,30 @@
 package Project;
 
 public class DateMain {
+	
+	public static void outputSort(Date[] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	public static void sort(Date[] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			for(int j = 0; j < arr.length - 1; j++)
+			{
+				if(arr[j].compareTo(arr[j + 1]) == 1)
+				{
+					Date temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		outputSort(arr);
+	}
 
 	public static void main(String[] args) {
 		Date newDate = new Date(2, 5, 6, 10, 2, 2020);
