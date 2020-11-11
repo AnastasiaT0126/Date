0856f8810286e9e3b972288860fb59039a139215
Anastasia Todorova <anito0401@gmail.com>
2020-11-11 17:24:41 +0200

    Reminder

---
+++ src/Project/Reminder.java
@@ -0,0 +1,64 @@
+package Project;
+
+import java.time.LocalDate;
+
+public class Reminder {
+	private static int eventsSize = 30;
+	
+	private Event[] events;
+	private int eventsIndex;
+	
+	public Reminder()
+	{
+		events = new Event[eventsSize];
+		eventsIndex = 0;
+	}
+	
+	public void addEvent(Event newEvent)
+	{
+		if(eventsIndex >= eventsSize)
+		{
+			System.out.println("Your reminder is full");
+		}
+		if(eventsIndex < eventsSize)
+		{
+			events[eventsIndex] = newEvent;
+			eventsIndex++;
+		}
+	}
+	
+	public Event[] getAllEvents(Date date)
+	{
+		int todaysEvents = 0;
+		
+		for(int i = 0; i < eventsIndex; i++)
+		{
+			if(events[i].getDate().equalDates(date) == true)
+			{
+				todaysEvents++;
+			}
+		}
+		Event[] todaysEventsArr = new Event[todaysEvents];
+		
+		Date currentDate;
+		int index = 0;
+		
+		for(int i = 0; i < eventsIndex; i++)
+		{
+			currentDate = events[i].getDate();
+			if(date.equalDates(currentDate) == true)
+			{
+				todaysEventsArr[index] = new Event();
+				todaysEventsArr[index] = events[i];
+				index++;				
+			}
+		}
+		return todaysEventsArr;
+	}
+	
+	public void remind()
+	{
+		LocalDate currentDate = LocalDate.now(); 
+	}
+
+}
